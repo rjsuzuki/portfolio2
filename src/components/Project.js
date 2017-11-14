00c7569d1b6536d86d8be3f62b6c501c28fcb55c
RJ <ryanjsuzuki@gmail.com>
2017-11-14 01:28:40 +0000

    Update portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,8 @@ class Project extends React.Component {
             <Shuffle className="col-lg-4 col-md-4 col-sm-6 col-xs-12" id="shuffle-item item">
                   <div className="panel panel-default">
                     <div className="panel-heading">{this.props.title}</div>
-                    <div className="panel-body"><img src={this.props.imgSrc} alt={this.props.alt} width={300} height={150} mode="fit"/></div>
+                    <div className="panel-heading">{this.props.info}</div>
+                    <div className="panel-body"><a href={this.props.url} target="new"><img src={this.props.imgSrc} alt={this.props.alt} width={300} height={150} mode="fit"/></a></div>
                   </div>
             </Shuffle>
         );
--- src/components/ShuffleView.js
+++ src/components/ShuffleView.js
@@ -8,13 +8,11 @@ class ShuffleView extends React.Component {
   render() {
     return (
         <div>
-
             <div className="row shuffle-grid" id="grid">
-                <Project title="Northern California Karate" imgSrc={projectimg1} alt="nck" data-groups='["web"]'/>
-                <Project title="Cygna Energy Services" imgSrc={projectimg2} alt="cygna" data-groups='["web"]'/>
-                <Project title="Flappy Bird Clone" imgSrc="http://placehold.it/300x100" alt="flappy" data-groups='["misc"]'/>
-                <Project title="Dotfiles" imgSrc="http://placehold.it/300x100" alt="dotfiles" data-groups='["misc"]' />
-                
+                <Project title="Northern California Karate" info="Rails + AWS S3 + Heroku + Postgresql" imgSrc={projectimg1} alt="nck" url="http://norcalkaratedo.orgs" data-groups='["web"]'/>
+                <Project title="Cygna Energy Services" info="Bootstrap + JS + Spring MVC" imgSrc={projectimg2} alt="cygna" url="http://cygna.net" data-groups='["web"]'/>
+                <Project title="Flappy Bird Clone" info="Unity + C#" imgSrc="http://placehold.it/300x100" url="http://github.com/rjsuzuki/flappybird-clone-demo" alt="flappy" data-groups='["misc"]'/>
+                <Project title="Dotfiles" info="A simple dotfile setup" imgSrc="http://placehold.it/300x100" alt="dotfiles" url="http://github.com/rjsuzuki/dotfiles" data-groups='["misc"]' />
             </div>
         </div>
     );
